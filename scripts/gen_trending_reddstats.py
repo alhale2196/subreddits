34f1bdd268dd3159772e1872c87f85c81609189a
Andrew Hale <halea2196@gmail.com>
2025-07-24 01:42:50 -0400

    Added NSFW subreddit scraping

--- scripts/gen_trending_reddstats.py
+++ scripts/gen_trending_reddstats.py
@@ -7,16 +7,15 @@ from requests.exceptions import RequestException, Timeout, HTTPError
 BASE_URL = 'https://reddstats.com/ranking/relative'
 PERIODS = ['daily', 'weekly']
 SUBSCRIBER_BINS = ['10001-50000', '50001-100000', '100001-1000000']
-LIMIT_PER_BIN = 7
 REQUEST_TIMEOUT = 90
 
-def generate_trending(period: str) -> None:
+def generate_trending(period: str, limit: int, over18: bool = False) -> None:
     if period not in PERIODS:
         raise ValueError(f"Invalid period type. Must be one of {PERIODS}")
 
     subreddits = []
     for bin in SUBSCRIBER_BINS:
-        url = f'{BASE_URL}?over18=False&period={period}&subscriber_classification={bin}'
+        url = f'{BASE_URL}?over18={str(over18)}&period={period}&subscriber_classification={bin}'
         try:
             response = requests.get(url, timeout=REQUEST_TIMEOUT)
             response.raise_for_status()
@@ -31,14 +30,20 @@ def generate_trending(period: str) -> None:
 
         soup = BeautifulSoup(response.content, 'html.parser')
         subreddit_elements = soup.select('div.item a[href^="/subreddit/"]')
-        for element in subreddit_elements[:LIMIT_PER_BIN]:
+        for element in subreddit_elements[:limit]:
             subreddits.append(element.text.strip())
     
     for subreddit in subreddits:
         print(subreddit)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate trending subreddits.')
     parser.add_argument('period', type=str, help='The period type (\'daily\' or \'weekly\')')
+    parser.add_argument("--nsfw", help="Whether to include NSFW subreddits", action="store_true")
+    parser.add_argument("--limit", help="Number of subreddits to return per bin", type=int, default=7)
     args = parser.parse_args()
-    generate_trending(args.period)
+    if args.nsfw:
+        generate_trending(args.period, limit=args.limit, over18=args.nsfw)
+    else:
+        generate_trending(args.period, limit=args.limit)
